e0da412d310fa1015c82a16c17e0b1775ade1a54
Kalininskiy Andrei <kresthaus@mail.ru>
2022-06-19 18:59:58 +0400

    Start

---
+++ Home №3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов случайными числами и выводит их на экран.
+
+Console.Clear();
+ int[] myArray = new int[8];
+         Random rand = new Random();
+            
+         for (int x = 0; x < myArray.Length; x++)
+            {
+                myArray[x] = rand.Next(100);
+                Console.WriteLine("Значение элемента массива " + x + " = " + myArray[x]);
+            }
